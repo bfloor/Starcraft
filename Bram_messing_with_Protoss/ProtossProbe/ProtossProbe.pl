e3489a23a9601d44daff1182b5ad44efa1e9837a
Bram <bram_durieux@yahoo.com>
2017-01-31 17:43:21 +0100

    Now with the proper files

--- Bram_messing_with_Protoss/ProtossProbe/ProtossProbe.pl
+++ Bram_messing_with_Protoss/ProtossProbe/ProtossProbe.pl
@@ -7,11 +7,14 @@ mineralField/4,
 constructionSite/3,
 workerActivity/2,
 self/2,
-chokepoint/2,
 base/4,
 unitAmount/2,
 supply/2,
-nexusPosition/2.
+nexusPosition/2,
+isBuilder/1,%% DIY believes, goals
+vespeneGeyser/4,
+probeStatus/3,
+assimilator/1.
 
 	% Indicates when agent is busy gathering
 	busy :- condition(List), member(gathering, List).
@@ -20,14 +23,18 @@ nexusPosition/2.
 	cost("Protoss Pylon", 100, 0).
 	cost("Protoss Nexus", 400, 0).
 	cost("Protoss Gateway", 150, 0).
-						
+	cost("Protoss Assimilator", 150, 0).
+							
 	% default distance check				
 	distance(X1,Y1,X2,Y2,D) :- D is sqrt((X2-X1)**2 + (Y2-Y1)**2).
 				
-	% example code for gas gathering handling
-	shouldMineVespene :- unitAmount("Protoss Assimilator",RefCount),
-			     NeededWorkers is RefCount * 2,
-			     aggregate_all(count, workerActivity(_,gatheringGas), Count),
-			     Count < NeededWorkers.
+	% example code for gas gathering handling 	%
+
+	enoughAssimilators :- aggregate_all(count, assimilator(_), Count),
+			     Count > 0.
+
 	
-						
+	shouldMineVespene :- 	aggregate_all(count, workerActivity(_,gatheringGas), Count),
+			     	Count < 2,
+			     	enoughAssimilators,
+			     	not(builder).
