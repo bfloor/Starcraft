165dcad7329cd5757f63c5b7d38392b66fb7f1ab
Bram <Bram@grover.sesam.straat>
2017-02-02 10:37:48 +0100

    Added screenshots and log of MAS winning!

---
+++ Deliverables/MAS/ProtossNexus/ProtossNexus.pl
@@ -0,0 +1,16 @@
+:- dynamic
+self/2,
+queueSize/1,
+friendly/4,
+isMorphing/2,
+minerals/1,
+unitAmount/2,
+gas/1,
+supply/2,
+train/1,
+probe/2,
+done/2,
+nexusPosition/2,
+ready/1.
+
+allReady :- aggregate_all(count, ready(_), Count), Count > 5.
